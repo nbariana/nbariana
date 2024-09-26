210e1f60af20624e8fba854344d78df3e44393fa
nbariana <nbariana@my.bcit.ca>
2024-09-26 15:31:41 -0700

    fixing accidental branch issue

--- stables.js
+++ stables.js
@@ -13,5 +13,9 @@ console.log(STABLE_MONTHLY_FEE);
 let horseAgeIn10Years = horseAge = 10;
 console.log(horseAgeIn10Years);
 
-// multiplication operator = *
-// divison operator =/
+
+const STABLE_MONTHLY_FEE = 100;
+let horseIntroduction = "is the name of myhorse";
+let rentIntroduction = "it costs" +STABLE_MONTHLY_FEE + "to board" +horseName;
+let stableIntroduction ="Welcome to" + horseName + "'s stable";
+// console.log(stableIntroduction)
