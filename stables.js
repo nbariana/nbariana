51f7480b555fff79307900cb7d6e9018d192be67
nbariana <nbariana@my.bcit.ca>
2024-09-26 16:25:01 -0700

    lab 4 homework

--- stables.js
+++ stables.js
@@ -1,6 +1,6 @@
 console.log("horse")
-let horseName = "bubs"
-horseName = "fresa"
+
+let horseName = "fresa"
 let horseAge = 2
 console.log(horseName)
 console.log(horseAge)
@@ -14,8 +14,22 @@ let horseAgeIn10Years = horseAge = 10;
 console.log(horseAgeIn10Years);
 
 
-const STABLE_MONTHLY_FEE = 100;
-let horseIntroduction = "is the name of myhorse";
+let horseIntroduction = "This is my horse," + horseName;
 let rentIntroduction = "it costs" +STABLE_MONTHLY_FEE + "to board" +horseName;
 let stableIntroduction ="Welcome to" + horseName + "'s stable";
 // console.log(stableIntroduction)
+
+
+//lesson4//
+console.log(horseIntroduction);
+console.log(rentIntroduction);
+
+let stableIntroduction = "Welcome to" + horseName   + "'s stable";
+console.log(stableIntroduction);
+console.log(horseIntroduction);
+let stableRent = "The rent at the stable is" + (STABLE_MONTHLY_FEE*3) + "for three months";
+console.log(stableRent);
+let stableDiscount = "a three month stay has a discount of" + ((STABLE_MONTHLY_FEE*3)*0.10);
+console.log(stableDiscount)
+let rentTotal = "Fresa paid $270 for rent.";
+console.log(rentTotal)
