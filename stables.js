20930fc1e77322dd10ca681e2dfb19d3a87a46bb
nbariana <nbariana@my.bcit.ca>
2024-09-26 15:24:59 -0700

    adding operators

---
+++ stables.js
@@ -0,0 +1,17 @@
+console.log("horse")
+let horseName = "bubs"
+horseName = "fresa"
+let horseAge = 2
+console.log(horseName)
+console.log(horseAge)
+let isHorseInStable = false;
+console.log(isHorseInStable)
+isHorseInStable = false;
+
+const STABLE_MONTHLY_FEE = 100;
+console.log(STABLE_MONTHLY_FEE);
+let horseAgeIn10Years = horseAge = 10;
+console.log(horseAgeIn10Years);
+
+// multiplication operator = *
+// divison operator =/
